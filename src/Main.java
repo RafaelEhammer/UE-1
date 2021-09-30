9055047d44e06de7a1f702eb78af49f8e7bd4d6a
Rafael Ehammer <Rafael.ehammer@htl-wels.at>
2021-09-30 14:33:32 +0200

    finished implementation

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 public class Main
 {
+    public static void main(String[] args)
+    {
+        DatePrinter DP = new DatePrinter();
+        Thread th1 = new Thread(DP);
+
+        CountPrinter CP = new CountPrinter();
+        Thread th2 = new Thread(CP);
+
+        th1.start();
+        th2.start();
+    }
 
 }
